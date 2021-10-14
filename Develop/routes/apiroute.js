d71b5368fe34ff263e870c8a027f89fb59990d40
Aniesia Schmolesky <aniesiaschmolesky@gmail.com>
2021-10-13 20:25:40 -0700

    added the workout range route

--- Develop/routes/apiroute.js
+++ Develop/routes/apiroute.js
@@ -19,6 +19,24 @@ app.get("/api/workouts", (req, res) => {
       res.json(err);
   });     
 });
+
+app.get("/api/workouts/range", (req, res) => {
+      Exercise.aggregate([
+            {
+                  $addFields: {
+                        totalDuration: {
+                              $sum:'$exercises.duration',
+                        },
+                  },
+            },
+      ])
+      .then((dbWorkout) => {
+         res.json(dbWorkout);
+     })
+     .catch((err) => {
+         res.json(err);
+     });     
+   });
   
 app.get("/exercise", function(req, res) {
    res.sendFile(path.join(__dirname, "../public/exercise.html"));
