83e08469e5790def45d89aa8a5ca462517548165
Aniesia Schmolesky <aniesiaschmolesky@gmail.com>
2021-10-13 19:47:02 -0700

    added workout model

---
+++ Develop/models/workout.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+      type: Date,
+      default: Date.now
+    },
+    exercises: [
+        {
+          type: {
+            type: String,
+            trim: true,
+            required: true
+          },
+          name: {
+            type: String,
+            trim: true,
+            required: true
+          },
+          weight: {
+            type: Number
+          },
+          sets: {
+            type: Number
+          },
+          reps: {
+            type: Number
+          },
+          distance: {
+            type: Number
+          },
+          duration: {
+            type: Number
+          }
+      }
+    ]
+  });
+  
+  const Workout = mongoose.model("workout", WorkoutSchema);
+  
+  module.exports = Workout;
