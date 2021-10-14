8551ebe342192c1949561eac6280e1ee978cdfa9
Aniesia Schmolesky <aniesiaschmolesky@gmail.com>
2021-10-13 20:21:07 -0700

    updated server.js

--- Develop/routes/apiroute.js
+++ Develop/routes/apiroute.js
@@ -1,9 +1,24 @@
 const path = require("path");
 const app = require("express").Router();
+const Exercise = require("../models");
 
-
-
-    
+app.get("/api/workouts", (req, res) => {
+   Exercise.aggregate([
+         {
+               $addFields: {
+                     totalDuration: {
+                           $sum:'$exercises.duration',
+                     },
+               },
+         },
+   ])
+   .then((dbWorkout) => {
+      res.json(dbWorkout);
+  })
+  .catch((err) => {
+      res.json(err);
+  });     
+});
   
 app.get("/exercise", function(req, res) {
    res.sendFile(path.join(__dirname, "../public/exercise.html"));
---
+++ Develop/server.js
@@ -0,0 +1,25 @@
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.use(require("./routes/apiroute.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
